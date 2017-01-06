262b50c7bef086b80dc634b02037bc4f2ded1fe9
Dennis Grabowski <dennis.grabowski@stud.hs-hannover.de>
2017-01-06 17:42:39 +0100

    add assertion that customer can create a booking if not on blacklist

--- src/test/java/hsh/master/exercise/str/BlacklistTest.java
+++ src/test/java/hsh/master/exercise/str/BlacklistTest.java
@@ -1,13 +1,17 @@
 package hsh.master.exercise.str;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -16,13 +20,26 @@ public class BlacklistTest {
     @Rule public MockitoRule mockrule = MockitoJUnit.rule();
     @Mock private Blacklist bl;
 
+    private Address a;
+    private Customer c;
+
+    @Before
+    public void init() {
+        a = new Address("de", "hannover", 30459, "Ricklinger Stadtweg", 120);
+        c = new Customer("Maren Sandner", a);
+    }
+
     @Test
     public void shouldRejectBookingWhenCustomerIsOnBlacklist() {
         when(bl.isOnBlacklist(anyString())).thenReturn(true);
-        Address a = new Address("de", "hannover", 30459, "Ricklinger Stadtweg", 120);
-        Customer c = new Customer("Maren Sandner", a);
         assertTrue(bl.isOnBlacklist(c.getName()));
         verify(bl).isOnBlacklist(c.getName());
     }
 
+    @Test
+    public void shouldAllowBookingWhenCustomerIsNotOnBlacklist() {
+        when(bl.isOnBlacklist(anyString())).thenReturn(false);
+        assertFalse(bl.isOnBlacklist(c.getName()));
+        verify(bl).isOnBlacklist(c.getName());
+    }
 }
